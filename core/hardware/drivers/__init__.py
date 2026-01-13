29231611bcb0705ac4a7fd08a9385aaebaa2aa4b
Mars375 <94058042+Mars375@users.noreply.github.com>
2026-01-13 20:45:22 +0100

    feat: add drivers __init__ for exports

--- core/hardware/drivers/__init__.py
+++ core/hardware/drivers/__init__.py
@@ -1,9 +1,7 @@
-from core.hardware.drivers.pca9685 import PCA9685
-from core.hardware.drivers.adc import ADC
-from core.hardware.drivers.imu import MPU6050
+"""Hardware drivers implementations"""
+
+from .pca9685_servo import PCA9685ServoController
 
 __all__ = [
-    "PCA9685",
-    "ADC",
-    "MPU6050"
+    "PCA9685ServoController",
 ]
