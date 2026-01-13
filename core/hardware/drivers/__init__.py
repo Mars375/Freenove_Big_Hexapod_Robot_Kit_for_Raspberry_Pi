e91336b39737b5d32298521a5e035d927686f283
Mars375 <94058042+Mars375@users.noreply.github.com>
2026-01-13 20:55:53 +0100

    feat: export MockServoController in drivers __init__

--- core/hardware/drivers/__init__.py
+++ core/hardware/drivers/__init__.py
@@ -1,7 +1,9 @@
 """Hardware drivers implementations"""
 
 from .pca9685_servo import PCA9685ServoController
+from .mock_servo import MockServoController
 
 __all__ = [
     "PCA9685ServoController",
+        "MockServoController",
 ]
